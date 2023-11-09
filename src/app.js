5b1a35c92a6a46df2c4bcf4157c5c59c6f13875c
Nelson Vasquez <nelson10846@gmail.com>
2023-11-09 02:17:17 -0300

    pf_nvasquez

--- src/app.js
+++ src/app.js
@@ -125,7 +125,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", chatController.handleUserDisconnection);
 });
 
-const PORT = config.port;
+const PORT = config.port||8080;
 
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
